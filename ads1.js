0de0b562739c36c219d50ac957dfa4b74f469cd6
theflazweb <96344367+theflazweb@users.noreply.github.com>
2022-11-15 09:59:32 +0700

    Add files via upload

---
+++ ads1.js
@@ -0,0 +1,9 @@
+
+atOptions = {
+  'key' : '5eea7a98e64830f9bdcefe7558864b93',
+  'format' : 'iframe',
+  'height' : 250,
+  'width' : 300,
+  'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://disposalsirbloodless.com/5eea7a98e64830f9bdcefe7558864b93/invoke.js"></scr' + 'ipt>');
---
+++ ads2.js
@@ -0,0 +1,8 @@
+atOptions = {
+'key' : 'a480e15ecf1de575401aeed4d9d2a1f5',
+'format' : 'iframe',
+'height' : 600,
+'width' : 160,
+'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://disposalsirbloodless.com/a480e15ecf1de575401aeed4d9d2a1f5/invoke.js"></scr' + 'ipt>');
